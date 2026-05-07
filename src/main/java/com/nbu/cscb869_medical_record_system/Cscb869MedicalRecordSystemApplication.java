c2261a492f983563edc9dd5a7d40110b29783120
vd-Dimitrov <vladislav.d.dimit@gmail.com>
2026-05-07 09:46:48 +0300

    Initial Commit

---
+++ src/main/java/com/nbu/cscb869_medical_record_system/Cscb869MedicalRecordSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.cscb869_medical_record_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscb869MedicalRecordSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cscb869MedicalRecordSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbu/cscb869_medical_record_system/Cscb869MedicalRecordSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.cscb869_medical_record_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscb869MedicalRecordSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
