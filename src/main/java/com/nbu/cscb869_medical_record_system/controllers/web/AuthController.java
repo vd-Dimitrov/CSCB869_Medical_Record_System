372786b21c93575bff0db3e8e3101a63085eefdf
vd-Dimitrov <vladislav.d.dimit@gmail.com>
2026-05-18 14:43:10 +0300

    Added authcontroller and DoctorController

---
+++ src/main/java/com/nbu/cscb869_medical_record_system/controllers/web/AuthController.java
@@ -0,0 +1,13 @@
+package com.nbu.cscb869_medical_record_system.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
